f2874906e23d67ab483c6c3a1a8e8ddb2dc4844f
SkyeBeFreeman <928016560@qq.com>
2018-01-04 15:53:25 +0800

    update private key

--- dockerfile-jenkins/run.sh
+++ dockerfile-jenkins/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# git clone https://TuSimpleDevOps:tusimple2017@github.com/TuSimple/infra-jenkins-config.git
-# cp -r infra-jenkins-config/jenkins-backup/F* /tmp/backups/
+git clone https://E-Help:ehelp2016?gitlab.com/ehelper/devops/jenkins-config.git
+cp -r jenkins-config/jenkins-backup/F* /tmp/backups/
 
 java -jar /jenkins.war --httpPort=4843
