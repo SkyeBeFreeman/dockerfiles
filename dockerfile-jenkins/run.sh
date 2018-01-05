1082fb6f518188ebc858e9e5cb375d64a98385db
SkyeBeFreeman <928016560@qq.com>
2018-01-05 11:13:22 +0800

    update

--- dockerfile-jenkins/run.sh
+++ dockerfile-jenkins/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git clone https://E-Help:ehelp2016?gitlab.com/ehelper/devops/jenkins-config.git
+git clone https://E-Help:ehelp2016@gitlab.com/ehelper/devops/jenkins-config.git
 cp -r jenkins-config/jenkins-backup/F* /tmp/backups/
 
 java -jar /jenkins.war --httpPort=4843
