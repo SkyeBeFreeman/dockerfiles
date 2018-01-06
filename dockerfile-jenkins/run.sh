bdd8b8bbf3cbe81d991a38d38314d5dc67af2210
SkyeBeFreeman <928016560@qq.com>
2018-01-06 16:27:09 +0800

    update run.sh of jenkins

--- dockerfile-jenkins/run.sh
+++ dockerfile-jenkins/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+git config --global user.name "E-Help"
+git config --global user.name "E-Help"
+
 git clone https://E-Help:ehelp2016@gitlab.com/ehelper/devops/jenkins-config.git
 cp -r jenkins-config/jenkins-backup/F* /tmp/backups/
 
